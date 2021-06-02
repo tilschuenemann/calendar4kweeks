ce7128477ca36138840dff702093a7cdd9d2e2c2
tilschuenemann <til.schuenemann@mailbox.org>
2021-06-02 10:58:19 +0200

    removed custom color support

--- R/create_calendar.R
+++ R/create_calendar.R
@@ -2,8 +2,6 @@
 #' Create a 4k weeks calendar!
 #'
 #' @param birthdate your birthdate, format: yyyy-mm-dd
-#' @param passed_color rgb color for passed weeks
-#' @param col_future rgb color for future weeks
 #'
 #' @return 4k weeks calendar as ggplot object
 #' @export
@@ -27,37 +25,35 @@
 #' @importFrom graphics title
 #' @importFrom lubridate ymd
 #'
-create_calendar <- function(birthdate, passed_color = "#000000", future_color = "#000000") {
+create_calendar <- function(birthdate) {
 
   # debugging
   # birthdate <- as.Date("2000-01-01")
-  # passed_color <- "#00ff00"
-  # future_color <- "#ff0000"
-  #birthdate <- "test"
+  # birthdate <- "test"
 
-  result = tryCatch({
-    ymd(birthdate)
-  }, warning = function(w) {
-    stop("birthdate must be a date")
-  }, error = function(e) {
-    stop("birthdate must be a date")
-  })
+  result <- tryCatch(
+    {
+      ymd(birthdate)
+    },
+    warning = function(w) {
+      stop("birthdate must be a date")
+    },
+    error = function(e) {
+      stop("birthdate must be a date")
+    }
+  )
 
   birthdate <- ymd(birthdate)
 
   # check for correct inputs
   if (!is.Date(birthdate)) {
     stop("birthdate must be a date")
-  } else if (!grepl(pattern = "#{1}[0-9a-f]{6}", passed_color)) {
-    stop("col_past must be a RGB color")
-  } else if (!grepl(pattern = "#{1}[0-9a-f]{6}", future_color)) {
-    stop("col_past must be a RGB color")
   }
 
   # set colors
   mycol <- c(
-    "1" = passed_color,
-    "0" = future_color
+    "1" = "#000000",
+    "0" = "#000000"
   )
 
   # result df
@@ -83,7 +79,7 @@ create_calendar <- function(birthdate, passed_color = "#000000", future_color = 
       passed = as.factor(ifelse(id < as.integer(((today() - birthdate)) / 7), "1", "0"))
     )
 
-  font_size <- 12
+  font_size <- 10
 
   calplot <- ggplot(caldf, aes(week, year, color = passed, shape = passed)) +
     geom_point() +
@@ -97,10 +93,10 @@ create_calendar <- function(birthdate, passed_color = "#000000", future_color = 
       panel.grid.minor = element_blank(),
       panel.border = element_blank(),
       axis.ticks = element_blank(),
-      plot.title = element_text(family = "serif", size = 20),
-      axis.title.x = element_text(family = "serif", hjust = 0, size = font_size),
-      axis.title.y = element_text(family = "serif", vjust = 1, angle = 0, size = font_size),
-      axis.text = element_text(family = "serif", )
+      plot.title = element_text(family = "serif", size = font_size * 2, face = "italic"),
+      axis.title.x = element_text(family = "serif", hjust = 0, size = font_size, face = "italic"),
+      axis.title.y = element_text(family = "serif", vjust = 1, angle = 0, size = font_size, face = "italic"),
+      axis.text = element_text(family = "serif", size = font_size, face = "italic", colour = "black")
     ) +
     scale_y_discrete(breaks = seq(5, 80, by = 5), limits = rev) +
     scale_x_discrete(breaks = seq(5, 80, by = 5), position = "top")
--- tests/testthat/test-create_calendar.R
+++ tests/testthat/test-create_calendar.R
@@ -1,21 +1,13 @@
 test_that("inputs are correct", {
   expect_error(
-    create_calendar("test", "#000000", "#000000"),
+    create_calendar("test"),
     "birthdate must be a date"
-  )
-  expect_error(
-    create_calendar("2021-01-01", "test", "#000000"),
-    "col_past must be a RGB color"
-  )
-  expect_error(
-    create_calendar("2021-01-01", "#000000", "test"),
-    "col_past must be a RGB color"
   )
 })
 
 test_that("output is ggplot", {
   expect_type(
-    create_calendar(as.Date("2021-01-01"), "#000000", "#000000"),
+    create_calendar(as.Date("2021-01-01")),
     "list"
   )
 })
